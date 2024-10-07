32c369ceb02c5b14ee1a3bf042bd38595eccf6fa
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-10-07 10:05:10 -0300

    feat: custom snackbar implementation

---
+++ app/lib/utils/messages.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Messages {
+  final BuildContext context;
+  Messages._(this.context);
+
+  factory Messages.of(BuildContext context) {
+    return Messages._(context);
+  }
+
+  void showError(String message) =>
+      _showMessage(message, context.colorScheme.error);
+
+  void showSuccess(String message) =>
+      _showMessage(message, context.colorScheme.primaryFixed);
+
+  void _showMessage(String message, Color color) {
+    var scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: color,
+        content: Text(message),
+        duration: const Duration(seconds: 5),
+        shape: const StadiumBorder(),
+        behavior: SnackBarBehavior.floating,
+        action: SnackBarAction(
+          label: 'OK',
+          textColor: Theme.of(context).colorScheme.onError,
+          onPressed: () {
+            scaffoldMessenger.hideCurrentSnackBar();
+          },
+        ),
+      ),
+    );
+  }
+}
+
+extension Te on BuildContext {
+  ColorScheme get colorScheme => Theme.of(this).colorScheme;
+}
