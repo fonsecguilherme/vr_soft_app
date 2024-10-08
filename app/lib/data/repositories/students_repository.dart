bc41141a73d2de62b78ac18345fbcd0ae40d0063
Guilherme Fonseca <43682827+fonsecguilherme@users.noreply.github.com>
2024-10-08 12:03:41 -0300

    feat: student repository abstraction and implementation

--- app/lib/data/repositories/students_repository.dart
+++ app/lib/data/repositories/students_repository.dart
@@ -1,27 +1,144 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:app/domain/repositories/i_students_repository.dart';
+import 'package:app/domain/service/i_http_client.dart';
+
+import '../../domain/repositories/i_course_repository.dart';
+import '../models/student.dart';
 
 class StudentsRepository implements IStudentRepository {
+  StudentsRepository({
+    required this.client,
+  });
+
+  final IHttpClient client;
+
+  final _baseUrl = 'http://localhost:8080/students';
+
   @override
-  Future<void> delete(int id) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<ApiResponse<List<Student>?>> getAll() async {
+    final url = '$_baseUrl/get_all';
+
+    try {
+      final response = await client.get(url);
+
+      if (response.statusCode == 200) {
+        final body = jsonDecode(response.body);
+
+        final List<Student> students = body
+            .map((e) => Student.fromJson(e as Map<String, dynamic>))
+            .toList();
+
+        return (data: students, error: Empty());
+      } else {
+        return (
+          data: null,
+          error: Failure(message: 'Erro ao obter lista de alunos')
+        );
+      }
+    } catch (error, stacktrace) {
+      log('error: $error\nstacktrace: $stacktrace');
+      return (
+        data: null,
+        error: Failure(message: 'Erro ao tentar se conectar com o servidor.')
+      );
+    }
   }
 
   @override
-  Future<void> getAll() {
-    // TODO: implement getAll
-    throw UnimplementedError();
+  Future<ApiResponse<String?>> save({required String name}) async {
+    final url = '$_baseUrl/save';
+
+    final body = {
+      "nome": name,
+    };
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      final response = await client.post(
+        url,
+        headers: headers,
+        body: jsonEncode(body),
+      );
+
+      if (response.statusCode == 201) {
+        return (data: 'Aluno registrado com sucesso.', error: Empty());
+      } else {
+        return (
+          data: null,
+          error: Failure(message: 'Erro ao registrar de aluno')
+        );
+      }
+    } catch (error, stacktrace) {
+      log('error: $error\nstacktrace: $stacktrace');
+      return (
+        data: null,
+        error: Failure(message: 'Erro ao tentar se conectar com o servidor.')
+      );
+    }
   }
 
   @override
-  Future<void> save(String name) {
-    // TODO: implement save
-    throw UnimplementedError();
+  Future<ApiResponse<String?>> delete({required int id}) async {
+    final url = '$_baseUrl/delete_by_id?id=$id';
+
+    try {
+      final response = await client.delete(url);
+      if (response.statusCode == 200) {
+        return (data: response.body, error: Empty());
+      } else if (response.statusCode == 404) {
+        return (data: null, error: Failure(message: response.body));
+      } else if (response.statusCode == 409) {
+        return (data: null, error: Failure(message: response.body));
+      } else {
+        return (
+          data: null,
+          error: Failure(message: 'Erro desconhecido ao tentar excluir aluno.')
+        );
+      }
+    } catch (error, stacktrace) {
+      log('error: $error\nstacktrace: $stacktrace');
+      return (
+        data: null,
+        error: Failure(message: 'Erro ao tentar se conectar com o servidor.')
+      );
+    }
   }
 
   @override
-  Future<void> update(int id, String name) {
-    // TODO: implement update
-    throw UnimplementedError();
+  Future<ApiResponse<String?>> update({
+    required int id,
+    required String name,
+  }) async {
+    final url = '$_baseUrl/update_by_id?id=$id';
+
+    final body = {
+      "nome": name,
+    };
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      final response = await client.put(
+        url,
+        headers: headers,
+        body: jsonEncode(body),
+      );
+
+      if (response.statusCode == 200) {
+        return (data: 'Editado com sucesso.', error: Empty());
+      } else if (response.statusCode == 404) {
+        return (data: null, error: Failure(message: response.body));
+      } else {
+        return (data: null, error: Failure(message: 'Erro desconhecido ao tentar vincular aluno.'));
+      }
+    } catch (e) {
+      return (data: null, error: Failure(message: 'Erro ao tentar se conectar com o servidor.'));
+    }
   }
 }
--- app/lib/domain/repositories/i_students_repository.dart
+++ app/lib/domain/repositories/i_students_repository.dart
@@ -1,6 +1,16 @@
+import 'package:app/data/models/student.dart';
+import 'package:app/domain/repositories/i_course_repository.dart';
+
 abstract class IStudentRepository {
-  Future<void> getAll();
-  Future<void> save(String name);
-  Future<void> update(int id, String name);
-  Future<void> delete(int id);
+  Future<ApiResponse<List<Student>?>> getAll();
+  Future<ApiResponse<String?>> save({
+    required String name,
+  });
+  Future<ApiResponse<String?>> delete({
+    required int id,
+  });
+  Future<ApiResponse<String?>> update({
+    required int id,
+    required String name,
+  });
 }
